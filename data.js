8316c72d1fee850d0e421addf139f38490e2b418
rmacl <42400395+rmacl@users.noreply.github.com>
2019-06-17 23:16:55 -0700

    Update data.js

--- data.js
+++ data.js
@@ -2,27 +2,25 @@ const mongoose = require('mongoose');
 const Makgoli  = require('./model/makgoli');
 
 const aMakgoli = [
-	{
-		title : 'seoul makgoli',		
+	{			
+		title : 'seoul makgoli',
+		area : 'Seoul',
 		food_suggestion : 'seafood pancake',
-		brewery : {
-			title : 'seoul brewery',
-			states : 'seoul',
-			address : 'Seoul seojeong-ro 33',
-			phone : '00-000-0000',
-			website : '',
+		brewery_id : 'sdfsdf',
+		image : 'https://catasy.cafe24.com//dummy/yuseong_star_makgoli.png',
+		is_sterilized : false,
+		description : 'Medium acidity, medium tannins and dry. Heavy body.',
+		alcohol_content : 5,
+		awards : 'best rice wine 2019',
+		ingredient : 'rice, sticky rice, sucralose, aspartame',
+		author : 'googoo@gmail.com',
+		is_claimed : false,
+	    updated : '',
+	    place : ['seoul restaurant','seoul bar'],
+	    rating : 4.5,
+	    sweetness : 3,
 
-		},
-		product : {
-			image : '',
-			is_sterilized : false, 
-			description : '',
-			alcohol_content : '5',
-			awards : '',
-			ingredient : 'rice, suclouse',
-		},
-	},
-];
+}];
 
 
 mongoose.connect('mongodb://localhost/makgoli' ,  { useNewUrlParser: true });
